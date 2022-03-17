c6a52d633075f2ef259ead4a4864fd3a1dfe1f48
Amber Frantz <amber.k.frantz@mgail.com>
2022-03-17 12:17:36 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution in this file!
+const employee = {
+    name: "Amber",
+    streetAddress: "1234 Wee Sing Lane"
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee, [key]:value}
+    return newEmployee
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+
+function deleteFromEmployeeByKey(employee, key){
+    employee = {...employee}
+    delete employee[key]
+    return employee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
+
+
+/*
+
+  describe('destructivelyDeleteFromEmployeeByKey(employee, key)', function () {
+    it('returns employee without the deleted key/value pair', function () {
+      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+
+      expect(newEmployee['name']).to.equal(undefined);
+    });
+
+    it('modifies the original employee', function () {
+      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+
+      expect(employee['name']).to.equal(undefined);
+      expect(employee).to.equal(newEmployee);
+    });
+  });
+});
+*/
